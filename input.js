4b3ced1b3fcbc07f7b82578ff312996c6efea930
Monica Palko <m.a.palko07@gmail.com>
2024-02-23 22:05:12 -0500

    -a

---
+++ input.js
@@ -0,0 +1,17 @@
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = function (data) {
+  if (data === "\u0003") {
+    process.exit();
+  }
+};
+const stdin = setupInput();
+stdin.on("data", handleUserInput);
+
+module.exports = { setupInput };
