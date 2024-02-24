2c6c1edb3334d5732db23b5ac03c7032c70a8d17
Monica Palko <m.a.palko07@gmail.com>
2024-02-23 21:47:03 -0500

    -a

--- play.js
+++ play.js
@@ -2,3 +2,19 @@ const { connect } = require('./client'); // Importing the connect function
 
 console.log("Connecting ...");
 connect();
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = function (data) {
+  if (data === "\u0003") {
+    process.exit();
+  }
+};
+const stdin = setupInput();
+stdin.on("data", handleUserInput);
