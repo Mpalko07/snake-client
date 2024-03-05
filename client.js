73ef082c1fa62fea841d69c7d9e35b8ccdec33af
Monica Palko <m.a.palko07@gmail.com>
2024-03-04 22:28:54 -0500

    -a

--- client.js
+++ client.js
@@ -10,14 +10,14 @@ const connect = () => {
   conn.setEncoding("utf8");
 
   conn.on('connect', () => {
-    console.log("Successfully connected to game server");
+    console.log("Successfully connected to game server\n Commands\n - w: To move up\n - a: To move down\n - s: To move left\n - d: To move right\n - 1: sssssee ya!\n - 2: nomnomnom\n - 3: ready to lose?\n - Control + C: to exit game");
     conn.write("Name: MAP");
   });
 
   conn.on("data", (data) => {
     console.log('server say: ', data)
   })
-  
+
   return conn;
 };
 
